ab7802dc09ff565a459ccfdf98ca1adfcdebf656
ChristophTabTeam <christoph@tabteam.media>
2022-03-21 16:03:12 +0100

    contact backend created

--- client/src/components/Contact/index.jsx
+++ client/src/components/Contact/index.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import '../../styles/Contact.scss'
-import SendIcon from '@mui/icons-material/Send';
-import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
-import TitleOutlinedIcon from '@mui/icons-material/TitleOutlined';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane } from '@fortawesome/free-regular-svg-icons'
 
 function Contact() {
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [subject, setSubject] = useState()
+    const [message, setMessage] = useState()
+    const [sent, setSent] = useState(false)
+    const nameRef = useRef()
+    const emailRef = useRef()
+    const subjectRef = useRef()
+    const messageRef = useRef()
+
+    function submitForm() {
+        var nameContent = nameRef.current.value
+        var emailContent = emailRef.current.value
+        var subjectContent = subjectRef.current.value
+        var messageContent = messageRef.current.value
+        setName(nameContent)
+        setEmail(emailContent)
+        setSubject(subjectContent)
+        setMessage(messageContent)
+        nameRef.current.value = null
+        emailRef.current.value = null
+        subjectRef.current.value = null
+        messageRef.current.value = null
+    }
+
     return (
         <div className='Contact' id='contact'>
             <div className='contact__first'>
@@ -23,30 +43,34 @@ function Contact() {
                     <p>
                         TabTeam arbeitet <span className='green uppercase'>digital</span> und <span className='green uppercase'>zuvorkommend</span>.
                     </p>
+                    {name}
+                    {email}
+                    {subject}
+                    {message}
                 </div>
             </div>
             <div className='contact__second'>
                 <div className='contact__iphone'>
-                        <form action='http://95.216.199.125:3000/contact' method='POST' className='contact__form'>
+                        <form onSubmit={submitForm} className='contact__form'>
                             <div className='contact__iphone-notch'>
                                 <div className='contact__iphone-notch-sensor'/>
                                 <div className='contact__iphone-notch-speaker'/>
                                 <div className='contact__iphone-notch-camera'/>
                             </div>
                             <label className='contact__form-input-label'>
-                                <input type="text" name='name' placeholder='Name' className='contact__form-item contact__input '/>
-                                <span className='contact__form-input-icon'><PersonIconSizing/></span>
+                                <input type="text" name='name' placeholder='Name' ref={nameRef} className='contact__form-item contact__input '/>
+                                <span className='contact__form-input-icon'></span>
                             </label>
                             <label className='contact__form-input-label'>
-                                <input type="email" name='email' placeholder='E-Mail' className='contact__form-item contact__input'/>
-                                <span className='contact__form-input-icon'><MailIconSizing/></span>
+                                <input type="email" name='email' placeholder='E-Mail' ref={emailRef} className='contact__form-item contact__input'/>
+                                <span className='contact__form-input-icon'></span>
                             </label>
                             <label className='contact__form-input-label'>
-                                <input type="text" name='subject' placeholder='Betreff' className='contact__form-item contact__input'/>
-                                <span className='contact__form-input-icon'><TitleIconSizing/></span>
+                                <input type="text" name='subject' placeholder='Betreff' ref={subjectRef} className='contact__form-item contact__input'/>
+                                <span className='contact__form-input-icon'></span>
                             </label>
                             <label>
-                                <textarea name='message' placeholder='Schicke den Papierflieger auf die Reise...' className='contact__form-item contact__textarea'/>
+                                <textarea name='message' placeholder='Schicke den Papierflieger auf die Reise...' ref={messageRef} className='contact__form-item contact__textarea'/>
                             </label>
                             <button type='submit' className='contact__submit-button'><FontAwesomeIcon icon={faPaperPlane} className='contact__icon'/></button>
                         </form>
@@ -57,55 +81,3 @@ function Contact() {
 }
 
 export default Contact
-
-function PersonIconSizing() {
-    const width = window.innerWidth
-    if (width < 300) {
-        return <PersonOutlineOutlinedIcon sx={{ fontSize: 20}}/>
-    } else if (width < 500) {
-        return <PersonOutlineOutlinedIcon sx={{ fontSize: 20}}/>
-    } else if (width < 900) {
-        return <PersonOutlineOutlinedIcon sx={{ fontSize: 30}}/>
-    } else {
-        return <PersonOutlineOutlinedIcon sx={{ fontSize: 40}}/>
-    }
-}
-
-function MailIconSizing() {
-    const width = window.innerWidth
-    if (width < 300) {
-        return <MailOutlineOutlinedIcon sx={{ fontSize: 20}}/>
-    } else if (width < 500) {
-        return <MailOutlineOutlinedIcon sx={{ fontSize: 20}}/>
-    } else if (width < 900) {
-        return <MailOutlineOutlinedIcon sx={{ fontSize: 30}}/>
-    } else {
-        return <MailOutlineOutlinedIcon sx={{ fontSize: 40}}/>
-    }
-}
-
-function TitleIconSizing() {
-    const width = window.innerWidth
-    if (width < 300) {
-        return <TitleOutlinedIcon sx={{ fontSize: 20}}/>
-    } else if (width < 500) {
-        return <TitleOutlinedIcon sx={{ fontSize: 20}}/>
-    } else if (width < 900) {
-        return <TitleOutlinedIcon sx={{ fontSize: 30}}/>
-    } else {
-        return <TitleOutlinedIcon sx={{ fontSize: 40}}/>
-    }
-}
-
-function SendIconSizing() {
-    const width = window.innerWidth
-    if (width < 300) {
-        return <SendOutlinedIcon sx={{ fontSize: 20}}/>
-    } else if (width < 500) {
-        return <SendOutlinedIcon sx={{ fontSize: 30}}/>
-    } else if (width < 900) {
-        return <SendOutlinedIcon sx={{ fontSize: 40}}/>
-    } else {
-        return <SendOutlinedIcon sx={{ fontSize: 50}}/>
-    }
-}
--- index.js
+++ index.js
@@ -1,6 +1,61 @@
 const express = require('express')
+const nodemailer = require('nodemailer')
+const cors = require('cors')
+const creds = require('./config')
 
 const app = express()
+const router = express.Router()
 
-const PORT = process.env.PORT || 5000
+const transport = {
+    service: 'Outlook365',
+    host: 'smtp.office365.com',
+    port: '587',
+    auth: {
+        user: creds.user,
+        pass: creds.pass
+    }
+}
+
+const transporter = nodemailer.createTransport(transport)
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log('Server is ready to take messages')
+    }
+})
+
+router.post('/send', (req, res, next) => {
+    var name = req.body.name
+    var email = req.body.email
+    var subject = req.body.subject
+    var message = req.body.message
+    var content= `Absender: ${name} \n E-Mail: ${email} \n Betreff: ${subject} \n Nachricht: ${message}`
+
+    var mail = {
+        from: name,
+        to: creds.recev,
+        subject: `Neue Kontaktanfrage über das Kontaktformular`,
+        text: content
+    }
+
+    transporter.sendMail(mail, (err, data) => {
+        if (err) {
+            require.json({
+                status: 'fail'
+            })
+        } else {
+            res.json({
+                status: 'success'
+            })
+        }
+    })
+})
+
+app.use(cors())
+app.use(express.json())
+app.use('/', router)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server is listening on Port ${PORT}`))
